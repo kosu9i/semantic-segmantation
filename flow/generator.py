13706087d0bb0f7bd255d49c0787115fd544ea33
tomoki.kosugi <tomoki.kosugi@brainpad.co.jp>
2019-01-28 15:26:26 +0000

    Kaggle-Carvana-Image-Masking-Challengeのkernelからgeneratorをまるパクリ

---
+++ flow/generator.py
@@ -0,0 +1,53 @@
+import cv2
+import numpy as np
+
+def train_generator():
+    while True:
+        for start in range(0, len(ids_train_split), batch_size):
+            x_batch = []
+            y_batch = []
+            end = min(start + batch_size, len(ids_train_split))
+            ids_train_batch = ids_train_split[start:end]
+            for id in ids_train_batch.values:
+                img = cv2.imread('../../data/train/{}.jpg'.format(id))
+                img = cv2.resize(img, (input_size, input_size))
+                mask = cv2.imread('../../data/train_masks/{}_mask.png'.format(id), cv2.IMREAD_GRAYSCALE)
+                mask = cv2.resize(mask, (input_size, input_size))
+                img = randomHueSaturationValue(img,
+                                               hue_shift_limit=(-50, 50),
+                                               sat_shift_limit=(-5, 5),
+                                               val_shift_limit=(-15, 15))
+                img, mask = randomShiftScaleRotate(img, mask,
+                                                   shift_limit=(-0.0625, 0.0625),
+                                                   scale_limit=(-0.1, 0.1),
+                                                   rotate_limit=(-0, 0))
+                img, mask = randomHorizontalFlip(img, mask)
+                mask = np.expand_dims(mask, axis=2)
+                x_batch.append(img)
+                y_batch.append(mask)
+            x_batch = np.array(x_batch, np.float32) / 255
+            y_batch = np.array(y_batch, np.float32) / 255
+            yield x_batch, y_batch
+
+
+def valid_generator():
+    while True:
+        for start in range(0, len(ids_valid_split), batch_size):
+            x_batch = []
+            y_batch = []
+            end = min(start + batch_size, len(ids_valid_split))
+            ids_valid_batch = ids_valid_split[start:end]
+            for id in ids_valid_batch.values:
+                img = cv2.imread('../../data/train/{}.jpg'.format(id))
+                img = cv2.resize(img, (input_size, input_size))
+                mask = cv2.imread('../../data/train_masks/{}_mask.png'.format(id), cv2.IMREAD_GRAYSCALE)
+                mask = cv2.resize(mask, (input_size, input_size))
+                mask = np.expand_dims(mask, axis=2)
+                x_batch.append(img)
+                y_batch.append(mask)
+            x_batch = np.array(x_batch, np.float32) / 255
+            y_batch = np.array(y_batch, np.float32) / 255
+            yield x_batch, y_batch
+
+
+
